13f93429239c5023768e2c866c1942dd7bd7bccc
Андрей хухарев <alf.master.mail@gmail.com>
2022-08-07 18:28:35 +0300

    finish

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string[] array = { "hello", "2", "world", ":-)" };
+string[] resultArray = new string[array.Length];
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resultArray[j] = array[i];
+        j++;
+    }
+}
+
+System.Console.WriteLine(string.Join("\t", resultArray));
